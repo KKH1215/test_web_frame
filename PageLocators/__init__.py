aa78a00b5f6974843735609beac1e1063e106321
tang <343577336@qq.com>
2020-05-19 14:12:41 +0800

    第一次创建项目

---
+++ PageLocators/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : __init__.py.py
+@Time    : 2020/5/18 16:33
+@Author  : xiaobin
+@Email   : 343577336@qq.com
+@Software: PyCharm
+"""
+
+
---
+++ PageObjects/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : __init__.py.py
+@Time    : 2020/5/18 16:32
+@Author  : xiaobin
+@Email   : 343577336@qq.com
+@Software: PyCharm
+"""
+
+
